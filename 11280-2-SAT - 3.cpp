ef39f0e7b57cb54a6ea7b413f58453e844c51355
K_Gs:D <rootachieve6053@gmail.com>
2022-04-04 16:42:45 +0900

    2022_4_4_

---
+++ 11280-2-SAT - 3.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include<map>
+#include <stack>
+#define MOD(x,y,z) (((x)%z)+((y)%z))%z
+#define MODF(x,y,z) (((x)%z)*((y)%z))%z
+using namespace std;
+vector<int> v[20002];
+int n, m;
+int id = 0;
+int nodeId[20002];
+vector<vector<int>> SCC;
+bool fin[20002];
+stack<int> s;
+int dfs(int node) {
+	nodeId[node] = ++id;
+	int Min = nodeId[node];
+	s.push(node);
+	for (int i = 0; i < v[node].size(); i++) {
+		if (nodeId[v[node][i]] == 0) {
+			Min = min(Min, dfs(v[node][i]));
+		}
+		else if (!fin[v[node][i]]) {
+			Min = min(Min, nodeId[v[node][i]]);
+		}
+	}
+
+	if (Min == nodeId[node]) {
+		vector<int> scc;
+		while (1) {
+			int t = s.top();
+			s.pop();
+			scc.push_back(t);
+			fin[t] = true;
+			if (t == node) {
+				break;
+			}
+		}
+
+		sort(scc.begin(), scc.end());
+		SCC.push_back(scc);
+	}
+	return Min;
+}
+int main() {
+	int a, b;
+	int t1, t2, t3, t4;//t1 t3 : false t2,t4 = true;
+	scanf("%d%d",&n,&m);
+
+	for (int i = 1; i <= m; i++) {
+		scanf("%d%d", &a, &b);
+		t1 = abs(a) * 2 - 1;//not
+		t2 = abs(a) * 2;
+		t3 = abs(b) * 2 - 1;//not
+		t4 = abs(b) * 2;
+		if (a > 0 && b > 0) {
+			v[t1].push_back(t4);
+			v[t3].push_back(t2);
+		}
+		else if (a > 0 && b < 0) {
+			v[t1].push_back(t3);
+			v[t4].push_back(t2);
+		}
+		else if (a < 0 && b >0) {
+			v[t3].push_back(t1);
+			v[t2].push_back(t4);
+		}
+		else {
+			v[t2].push_back(t3);
+			v[t4].push_back(t1);
+		}
+	}
+
+	for (int i = 1; i <= 2 * n; i++) {
+		if (nodeId[i] == 0) {
+			dfs(i);
+		}
+	}
+	bool err = false;
+	int c, d;
+	for (int i = 0; i < SCC.size(); i++) {
+		for (int j = 0; j < SCC[i].size() - 1; j++) {
+			c = (SCC[i][j] % 2 == 0 ? SCC[i][j] / 2 : (SCC[i][j] + 1) / 2);
+			d = (SCC[i][j + 1] % 2 == 0 ? SCC[i][j + 1] / 2 : (SCC[i][j + 1] + 1) / 2);
+			if (c == d) {
+				err = true;
+				break;
+			}
+		}
+		if (err) {
+			break;
+		}
+	}
+
+	if (err) {
+		printf("0");
+	}
+	else {
+		printf("1");
+	}
+
+}
