ce3794eca948f25d2e440fd1a3fa24a35663422f
K_Gs:D <rootachieve6053@gmail.com>
2022-06-16 23:43:28 +0900

    2022_6_15_

---
+++ 10807-개수 세기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cmath>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+#define MAX 1000001
+#define MOD 1000000000
+#define MODULAR(x,y) (((x)%MOD)+((y)%MOD))%MOD
+#define MODULARF(x,y) (((x)%MOD)*((y)%MOD))%MOD
+#define MODULARM(x,y) (((x)%MOD)_((y)%MOD))%MOD
+using namespace std;
+int arr[201];
+int main() {
+	int n;
+	cin>>n;
+	int a;
+	while (n--) {
+		cin>>a;
+		arr[a+100]++;
+	}
+
+	cin>>n;
+	cout<<arr[n+100];
+}
+	
