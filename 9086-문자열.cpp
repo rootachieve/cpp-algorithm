e179b645223c90fe43ca7a994fb7a311ce8313d3
K_Gs:D <rootachieve6053@gmail.com>
2022-07-03 23:51:05 +0900

    2022_7_2_

---
+++ 9086-문자열.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+#define MAX 1000001
+#define MOD 1000000007
+#define MODULAR(x,y) (((x)%MOD)+((y)%MOD))%MOD
+#define MODULARF(x,y) (((x)%MOD)*((y)%MOD))%MOD
+#define MODULARM(x,y) (((x)%MOD)_((y)%MOD))%MOD
+using namespace std;
+
+int n;
+int main() {
+	cin>>n;
+	string temp;
+	for (int i = 1; i <= n; i++) {
+		cin>>temp;
+		cout<<temp[0]<<temp[temp.size()-1]<<"\n";
+	}
+}
