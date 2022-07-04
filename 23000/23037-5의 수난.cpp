342f58525a954af8c5da22398de9737da2fae455
K_Gs:D <rootachieve6053@gmail.com>
2022-07-05 01:38:09 +0900

    2022_7_3_

---
+++ 23000/23037-5의 수난.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+#define MAX 1000001
+#define MOD 1000000007
+#define MODULAR(x,y) (((x)%MOD)+((y)%MOD))%MOD
+#define MODULARF(x,y) (((x)%MOD)*((y)%MOD))%MOD
+#define MODULARM(x,y) (((x)%MOD)_((y)%MOD))%MOD
+using namespace std;
+
+int n;
+int main() {
+	int sum = 0;
+	string in;
+	cin>>in;
+	for (int i = 0; i < 5; i++) {
+		int base = in[i] - 48;
+		sum += (base*base*base*base*base);
+	}
+
+	cout<<sum;
+}
